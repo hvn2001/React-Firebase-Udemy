c16f60a3e4b8ddcf1656497b54dba84561e19817
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-11 22:53:30 +0700

    Complex JavaScript in JSX - 2

--- educative/src/App.js
+++ educative/src/App.js
@@ -22,12 +22,18 @@ const list = [
 ];
 
 class App extends Component {
-
     render() {
         return (
             <div className="App">
                 {list.map(function (item) {
-                    return <div>{item.title}</div>;
+                    return (
+                        <div>
+                            <span><a href={item.url}>{item.title}</a></span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </div>
+                    );
                 })}
             </div>
         );
