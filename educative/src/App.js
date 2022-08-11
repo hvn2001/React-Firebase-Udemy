ad72aadc0e3c9fd86a44e471e16106199cc5ef26
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-11 10:55:21 +0700

    Meet the First React Component https://www.educative.io/courses/road-to-react-with-hooks/YMV4DL5Jrx9

--- educative/src/App.js
+++ educative/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
+const title = 'React';
 
 function App() {
     return (
         <div>
-            <h1> Hello React </h1>
+            <h1>Hello World</h1>
         </div>
-
     );
 }
 
