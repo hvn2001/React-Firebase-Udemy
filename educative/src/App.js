0cc88aa65f3a22576338e46841fddadfd89294bf
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-11 11:37:16 +0700

    React Controlled Components

--- educative/src/App.js
+++ educative/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = React.useState('');
+    const [searchTerm, setSearchTerm] = React.useState('React');
 
     const handleSearch = event => {
         setSearchTerm(event.target.value);
