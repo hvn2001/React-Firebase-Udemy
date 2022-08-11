1020d9ddb9b6eeb14a8bb0234474f13a5879de9e
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-11 11:12:17 +0700

    Meet Another React Component - List component can be used in the App component:

--- educative/src/App.js
+++ educative/src/App.js
@@ -10,7 +10,12 @@ const list = [
         objectID: 0,
     },
     {
-        points: 50,
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
     },
 ];
 
@@ -24,20 +29,24 @@ function App() {
 
             <hr/>
 
-            {list.map(function (item) {
-                return (
-                    <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                    </div>
-                );
-            })}
+            <List/>
         </div>
     );
+}
+
+function List() {
+    return list.map(function (item) {
+        return (
+            <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </div>
+        );
+    });
 }
 
 export default App;
