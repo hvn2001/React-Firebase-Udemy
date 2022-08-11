8a3db6409be3a97e3af238d0e972712b5dc606b6
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-11 23:22:24 +0700

    Composable Components

--- educative/src/App.js
+++ educative/src/App.js
@@ -55,7 +55,9 @@ class App extends Component {
                 <Search
                     value={searchTerm}
                     onChange={this.onSearchChange}
-                />
+                >
+                    Search
+                </Search>
                 <Table
                     list={list}
                     pattern={searchTerm}
@@ -68,14 +70,14 @@ class App extends Component {
 
 class Search extends Component {
     render() {
-        const {value, onChange} = this.props;
+        const {value, onChange, children} = this.props;
         return (
             <form>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={onChange}
-                />
+                {children} <input
+                type="text"
+                value={value}
+                onChange={onChange}
+            />
             </form>
         );
     }
