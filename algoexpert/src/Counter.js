12e8b2369cd09880afbcfe3d280c65362bd1243a
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-14 17:27:40 +0700

    7 Component Lifecycle - useLayoutEffect - sync may slow

--- algoexpert/src/Counter.js
+++ algoexpert/src/Counter.js
@@ -1,17 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 // Mount (initial render) -> update (re-render) -> unmount
 export default function Counter() {
     const [count, setCount] = useState(0);
     const [bool, setBool] = useState(false);
 
-    useEffect(() => {
-        console.log('count changed')
-        return () => console.log('cleanup count change');
+    // useEffect --> async
+    useLayoutEffect(() => {
+        if (count === 3) {
+            setCount(4);
+        }
     }, [count]);
 
     useEffect(() => {
         console.log('render')
     });
+    const startTime = new Date();
+    while (new Date() - startTime < 100) {
+    }
 
     return (
         <div className="counter">
