c52963a38e2a2cfdf0a8c5aef5cfd027af5b2374
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-14 17:20:41 +0700

    7 Component Lifecycle - useEffect

--- algoexpert/src/Counter.js
+++ algoexpert/src/Counter.js
@@ -1,8 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 // Mount (initial render) -> update (re-render) -> unmount
 export default function Counter() {
     const [count, setCount] = useState(0);
     const [bool, setBool] = useState(false);
+
+    useEffect(() => {
+        console.log('count changed')
+        return () => console.log('cleanup count change');
+    }, [count]);
+
+    useEffect(() => {
+        console.log('render')
+    });
+
     return (
         <div className="counter">
             <button onClick={() => setBool(!bool)}>Re-render</button>
