7a011c340bf496d09f1450d783a9230bdd50f2e1
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-15 15:51:47 +0700

    12 Performance - init render button very long

--- algoexpert/src/MyButton.js
+++ algoexpert/src/MyButton.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function MyButton(props) {
+    console.log('Redering MyButton');
+
+    const startTime = new Date();
+    while (new Date() - startTime < 1000) {
+    }
     return <button {...props} style={{color: 'red'}}/>
 }
