9a15f973ad6829f59b046b958c61635e1a866c6c
hoang.vu <hoang.vu@vincere.com>
2022-08-16 00:38:14 +0700

    17- Debugging React - init

--- algoexpert/src/App.js
+++ algoexpert/src/App.js
@@ -1,30 +1,22 @@
-import './App.css';
-import React, {useState, useRef, useEffect} from "react";
+import React, {useState} from "react";
 
 export default function App() {
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState('');
-
-    const prevCount = usePrevious(count);
-    const prevText = usePrevious(text);
-
     return (
         <>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <p>Count: {count}</p>
-            <p>Previous render count: {prevCount}</p>
-
-            <input value={text} onChange={event => setText(event.target.value)}/>
-            <p>Previous render text: {prevText}</p>
+            <Counter initialValue={5}/>
+            <Counter/>
         </>
     );
 }
 
-function usePrevious(value) {
-    const prevRef = useRef();
-    useEffect(() => {
-        prevRef.current = value;
-    }, [value]);
-
-    return prevRef.current;
+function Counter({initialValue = 0}) {
+    const [count, setCount] = useState(initialValue);
+    return (
+        <>
+            <button onClick={() => setCount(count + 1)}>
+                Increment
+            </button>
+            <p>Count: {count}</p>
+        </>
+    )
 }
