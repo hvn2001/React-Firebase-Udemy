8d7cb1026c7ea3b6c7eda53c332a6c460fed33be
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-15 16:01:11 +0700

    12 Performance - minimize how often components are rendering at all - 1

--- algoexpert/src/MyButton.js
+++ algoexpert/src/MyButton.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, {memo} from "react";
 
-export default function MyButton(props) {
+export default memo(function MyButton(props) {
     console.log('Redering MyButton');
-
     const startTime = new Date();
     while (new Date() - startTime < 1000) {
     }
     return <button {...props} style={{color: 'red'}}/>
-}
+});
