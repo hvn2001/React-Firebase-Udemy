c1d53d091f6ad8e95c34b7f6ada63e54a6eeb047
hoang.vu <hoang.vu@theaccessgroup.com>
2022-08-16 16:37:47 +0700

    16 Error Handling - ErrorBoundary

--- algoexpert/src/App.js
+++ algoexpert/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {Component} from "react";
 
 export default function App() {
     return (
         <>
             <h1>Hello World</h1>
-            <Buggy/>
+            <ErrorBoundary fallback={<h1>There was an error</h1>}>
+                <Buggy/>
+            </ErrorBoundary>
         </>
     );
 }
@@ -12,4 +14,19 @@ export default function App() {
 function Buggy() {
     throw new Error('error');
     return <h1>Buggy</h1>;
+}
+
+class ErrorBoundary extends Component {
+    state = {hasError: false};
+
+    static getDerivedStateFromError(error) {
+        return {hasError: true};
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return this.props.fallback;
+        }
+        return this.props.children;
+    }
 }
